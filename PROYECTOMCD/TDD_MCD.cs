ad371e3cd8edf536e38f671bbef3fe08fbcbca8b
DuckSherlock <alejandrohgv18@gmail.com>
2024-05-08 17:28:40 -0500

    ejercicioMcd

---
+++ PROYECTOMCD/TDD_MCD.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class CalculadoraMCD
+{
+    // Método para calcular el MCD de dos números utilizando el algoritmo de Euclides
+    private int MCD(int a, int b)
+    {
+        while (b != 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    // Método para calcular el MCD de cuatro números
+    public int MCDDeCuatroNumeros(int a, int b, int c, int d)
+    {
+        int mcdAB = MCD(a, b);
+        int mcdCD = MCD(c, d);
+        return MCD(mcdAB, mcdCD);
+    }
+}
+
